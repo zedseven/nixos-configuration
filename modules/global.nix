4d7286b74ea87d768bed463ff90237aa7831618c
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2023-12-11 00:28:21 -0500

    Remove `system.copySystemConfiguration`.

--- modules/global.nix
+++ modules/global.nix
@@ -49,9 +49,4 @@
   };
 
   programs.fish.enable = true;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
 }
