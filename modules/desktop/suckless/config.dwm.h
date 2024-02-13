ca7fc61c1e472af90aa52dd01afb9df39de51823
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2024-02-12 21:36:07 -0500

    Change `firefox` to `firefox-devedition` in the `dmenu` priority list.

--- modules/desktop/suckless/config.dwm.h
+++ modules/desktop/suckless/config.dwm.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,
-								  "-hp", "firefox,discord-better,keepass,obsidian,steam,clion,rider,webstorm,jetbrains-toolbox,mullvad-vpn,blender,calibre,qbittorrent,xfe,slock,shutdown-now,reboot",
+								  "-hp", "firefox-devedition,discord-better,keepass,obsidian,steam,clion,rider,webstorm,jetbrains-toolbox,mullvad-vpn,blender,calibre,qbittorrent,xfe,slock,shutdown-now,reboot",
 								  "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
