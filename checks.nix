7ec3675d8e82096ad066b7c8f85b3fc17e89b60f
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2024-04-11 19:35:35 -0400

    Clean up `checks.nix` slightly.

--- checks.nix
+++ checks.nix
@@ -5,6 +5,7 @@
 }: let
   checks = system: let
     pkgs = inputs.nixpkgs.legacyPackages.${system};
+    cpkgs = inputs.self.packages.${system};
 
     revision = inputs.self.rev or inputs.self.dirtyRev;
     checkPrefix = "check";
@@ -15,7 +16,7 @@
       formatting = pkgs.runCommand "${checkPrefix}-formatting-${runSuffix}" {} ''
         set -o errexit
 
-        ${inputs.self.packages.${system}.alejandra}/bin/alejandra --check ${inputs.self} | tee $out
+        ${cpkgs.alejandra}/bin/alejandra --check ${inputs.self} | tee $out
       '';
 
       lints = pkgs.runCommand "${checkPrefix}-lints-${runSuffix}" {} ''
