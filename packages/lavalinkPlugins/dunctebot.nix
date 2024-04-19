52f6abc676f9b5fbd5c4dac98b7eb62b5d481a0c
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2024-04-18 23:18:43 -0400

    Improve the packaging for Lavalink and its plugins based on what is done for `minecraft-server`.

--- packages/lavalinkPlugins/dunctebot.nix
+++ packages/lavalinkPlugins/dunctebot.nix
@@ -4,25 +4,26 @@
   fetchurl,
   ...
 }:
-stdenvNoCC.mkDerivation (
-  finalAttrs: {
-    pname = "lavalink-dunctebot"; # The name is ambiguous between `dunctebot` and `skybot` - `dunctebot` was chosen because that's what the configuration uses
-    version = "1.6.3";
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "lavalink-dunctebot"; # The name is ambiguous between `dunctebot` and `skybot` - `dunctebot` was chosen because that's what the configuration uses
+  version = "1.6.3";
 
-    src = fetchurl {
-      url = "https://github.com/DuncteBot/skybot-lavalink-plugin/releases/download/${finalAttrs.version}/skybot-lavalink-plugin-${finalAttrs.version}.jar";
-      hash = "sha256-4ZfcDTghCZEEbBW5QQoZFySa2dkPkCU6c30eTQSaRR0=";
-    };
+  src = fetchurl {
+    url = "https://github.com/DuncteBot/skybot-lavalink-plugin/releases/download/${finalAttrs.version}/skybot-lavalink-plugin-${finalAttrs.version}.jar";
+    hash = "sha256-4ZfcDTghCZEEbBW5QQoZFySa2dkPkCU6c30eTQSaRR0=";
+  };
 
-    buildCommand = ''
-      install -Dm644 $src $out/plugins/skybot-lavalink-plugin.jar
-    '';
+  buildCommand = ''
+    install -Dm644 $src $out/plugins/skybot-lavalink-plugin.jar
+  '';
 
-    meta = {
-      homepage = "https://github.com/DuncteBot/skybot-lavalink-plugin";
-      description = "Lavalink plugin adding support for several audio source managers";
-      maintainers = with lib.maintainers; [zedseven];
-      platforms = lib.platforms.all;
-    };
-  }
-)
+  dontUnpack = true;
+
+  meta = {
+    homepage = "https://github.com/DuncteBot/skybot-lavalink-plugin";
+    description = "Lavalink plugin adding support for several audio source managers";
+    maintainers = with lib.maintainers; [zedseven];
+    platforms = lib.platforms.all;
+    sourceProvenance = with lib.sourceTypes; [binaryBytecode];
+  };
+})
--- packages/lavalinkPlugins/lavasrc.nix
+++ packages/lavalinkPlugins/lavasrc.nix
@@ -4,26 +4,27 @@
   fetchurl,
   ...
 }:
-stdenvNoCC.mkDerivation (
-  finalAttrs: {
-    pname = "lavalink-lavasrc";
-    version = "4.0.1";
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "lavalink-lavasrc";
+  version = "4.0.1";
 
-    src = fetchurl {
-      url = "https://github.com/topi314/LavaSrc/releases/download/${finalAttrs.version}/lavasrc-plugin-${finalAttrs.version}.jar";
-      hash = "sha256-JpUEpLGrRkC5ODgx57xCO4vyeILFkpKtnYTmvcSs84o=";
-    };
+  src = fetchurl {
+    url = "https://github.com/topi314/LavaSrc/releases/download/${finalAttrs.version}/lavasrc-plugin-${finalAttrs.version}.jar";
+    hash = "sha256-JpUEpLGrRkC5ODgx57xCO4vyeILFkpKtnYTmvcSs84o=";
+  };
 
-    buildCommand = ''
-      install -Dm644 $src $out/plugins/lavasrc-plugin.jar
-    '';
+  buildCommand = ''
+    install -Dm644 $src $out/plugins/lavasrc-plugin.jar
+  '';
 
-    meta = {
-      homepage = "https://github.com/topi314/LavaSrc";
-      description = "A collection of additional Lavaplayer/Lavalink Sources";
-      license = lib.licenses.asl20;
-      maintainers = with lib.maintainers; [zedseven];
-      platforms = lib.platforms.all;
-    };
-  }
-)
+  dontUnpack = true;
+
+  meta = {
+    homepage = "https://github.com/topi314/LavaSrc";
+    description = "A collection of additional Lavaplayer/Lavalink Sources";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [zedseven];
+    platforms = lib.platforms.all;
+    sourceProvenance = with lib.sourceTypes; [binaryBytecode];
+  };
+})
