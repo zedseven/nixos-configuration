cd350ddb45c6c373a6a4b60d29e722cbcc77ae46
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2024-04-18 23:15:33 -0400

    Split Lavalink plugins into their own section in the custom package list.

--- packages/default.nix
+++ packages/default.nix
@@ -11,16 +11,19 @@
     inherit (pkgs) callPackage;
   in {
     name = system;
-    value = rec {
-      alejandra = callPackage ./alejandra {};
-      lavalink = callPackage ./lavalink.nix {};
-      lavalinkPlugin-dunctebot = callPackage ./lavalinkPlugins/dunctebot.nix {};
-      lavalinkPlugin-lavasrc = callPackage ./lavalinkPlugins/lavasrc.nix {};
-      minecraft-server-fabric = callPackage ./minecraft-server-fabric {};
-      purefmt = callPackage ./purefmt.nix {inherit alejandra;};
-      steam-no-whats-new = callPackage ./steam-no-whats-new.nix {};
-      tealdeer = callPackage ./tealdeer.nix {};
-    };
+    value =
+      rec {
+        alejandra = callPackage ./alejandra {};
+        lavalink = callPackage ./lavalink.nix {};
+        minecraft-server-fabric = callPackage ./minecraft-server-fabric {};
+        purefmt = callPackage ./purefmt.nix {inherit alejandra;};
+        steam-no-whats-new = callPackage ./steam-no-whats-new.nix {};
+        tealdeer = callPackage ./tealdeer.nix {};
+      }
+      // {
+        lavalinkPlugin-dunctebot = callPackage ./lavalinkPlugins/dunctebot.nix {};
+        lavalinkPlugin-lavasrc = callPackage ./lavalinkPlugins/lavasrc.nix {};
+      };
   };
 in
   builtins.listToAttrs (map packages hostSystems)
