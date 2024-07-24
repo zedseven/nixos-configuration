c885b242f1b468f3f0798aeef11cc9c4a9c606f7
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2024-07-23 20:59:28 -0400

    Set up `duradeus` with the configuration for the new monitors.

--- modules/desktop/4k.nix
+++ modules/desktop/4k.nix
@@ -9,13 +9,13 @@ in {
   options.custom.desktop.is4k = with lib; mkEnableOption "4K display customisations";
 
   config = lib.mkIf cfg {
-    services.xserver.dpi = 192;
+    services.xserver.dpi = 144;
 
     home-manager.users.${userInfo.username} = {
-      home.pointerCursor.size = 64;
+      home.pointerCursor.size = 48;
 
       xresources.properties = {
-        "Xft.dpi" = 192;
+        "Xft.dpi" = 144;
       };
     };
   };
