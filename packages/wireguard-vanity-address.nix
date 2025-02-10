3f4a2c2256023773048c77cfb40e180f430f1033
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2025-02-09 20:23:36 -0500

    Replace the `wireguard-vanity-address` package override with a full package definition, since the upstream definition was removed.

--- packages/wireguard-vanity-address.nix
+++ packages/wireguard-vanity-address.nix
@@ -1,28 +1,32 @@
 {
   lib,
+  stdenv,
   fetchFromGitHub,
-  wireguard-vanity-address,
+  rustPlatform,
   optimiseForNativeCpu ? true,
-  ...
 }:
-wireguard-vanity-address.overrideAttrs (
-  oldAttrs: let
-    src = fetchFromGitHub {
-      owner = "zedseven";
-      repo = "wireguard-vanity-address";
-      rev = "5a5fcf4f8fd25c83c1db5e6437b54eb89b342318";
-      hash = "sha256-HRJUdZ+C6TcQACv0Bv1hvq7u61D3qbNFcN16SgFHCyE=";
-    };
-  in {
-    inherit src;
-    doCheck = false;
-    cargoDeps = oldAttrs.cargoDeps.overrideAttrs {
-      inherit src;
-      outputHash = "sha256-xbbHrQ5Lsr0kNWdUrI6hP9Gf1b8rYkv+P3COaQM80wo=";
-    };
+rustPlatform.buildRustPackage rec {
+  pname = "wireguard-vanity-address";
+  version = "0.4.0";
 
-    # Make the compiled binary even faster - from my testing, this speeds it up by about 25%
-    # This breaks builds that are run on systems other than the one they were compiled for
-    RUSTFLAGS = lib.optionalString optimiseForNativeCpu "-C target-cpu=native";
-  }
-)
+  src = fetchFromGitHub {
+    owner = "zedseven";
+    repo = "wireguard-vanity-address";
+    rev = "5a5fcf4f8fd25c83c1db5e6437b54eb89b342318";
+    hash = "sha256-HRJUdZ+C6TcQACv0Bv1hvq7u61D3qbNFcN16SgFHCyE=";
+  };
+
+  cargoHash = "sha256-xbbHrQ5Lsr0kNWdUrI6hP9Gf1b8rYkv+P3COaQM80wo=";
+
+  # Make the compiled binary even faster - from my testing, this speeds it up by about 25%
+  # This breaks builds that are run on systems other than the one they were compiled for
+  RUSTFLAGS = lib.optionalString optimiseForNativeCpu "-C target-cpu=native";
+
+  meta = with lib; {
+    description = "Find Wireguard VPN keypairs with a specific readable string";
+    homepage = "https://github.com/zedseven/wireguard-vanity-address";
+    license = licenses.mit;
+    maintainers = with maintainers; [zedseven];
+    mainProgram = "wireguard-vanity-address";
+  };
+}
