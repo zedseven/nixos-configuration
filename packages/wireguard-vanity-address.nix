e584c273cf92b4b4f9baf2ca110d3d2d8cc00aa6
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2025-06-16 22:54:13 -0400

    Update `wireguard-vanity-address`.

--- packages/wireguard-vanity-address.nix
+++ packages/wireguard-vanity-address.nix
@@ -6,16 +6,16 @@
 }:
 rustPlatform.buildRustPackage {
   pname = "wireguard-vanity-address";
-  version = "0.4.0";
+  version = "0.4.1-alpha.0";
 
   src = fetchFromGitHub {
     owner = "zedseven";
     repo = "wireguard-vanity-address";
-    rev = "5a5fcf4f8fd25c83c1db5e6437b54eb89b342318";
-    hash = "sha256-HRJUdZ+C6TcQACv0Bv1hvq7u61D3qbNFcN16SgFHCyE=";
+    rev = "1e9c45d17772d34d38f2dfee2194d6993da57508";
+    hash = "sha256-AIa2qoqTTe73ye1ZgJsvs5aUa78Xc6db5i7fPgjRuyU=";
   };
 
-  cargoHash = "sha256-xbbHrQ5Lsr0kNWdUrI6hP9Gf1b8rYkv+P3COaQM80wo=";
+  cargoHash = "sha256-+7OMjIVIXtT/cR3I33C0CmeX7QHAZDmRrSbqH16LDzA=";
 
   # Make the compiled binary even faster - from my testing, this speeds it up by about 25%
   # This breaks builds that are run on systems other than the one they were compiled for
