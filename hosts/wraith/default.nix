279da8e90728ae56f7233756bc10cd384ec7e028
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2026-06-02 16:45:27 -0400

    Enable Virtualbox on `wraith`.

--- hosts/wraith/default.nix
+++ hosts/wraith/default.nix
@@ -153,6 +153,14 @@ in {
     zfs.enable = true;
   };
 
+  virtualisation.virtualbox.host = {
+    enable = true;
+    enableExtensionPack = true;
+  };
+  users.users.${userInfo.username}.extraGroups = [
+    "vboxusers"
+  ];
+
   networking = {
     hostId = "eff5369a";
     supplicant.${networkInterfaceWirelessName}.configFile.path =
